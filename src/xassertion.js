5f68b7d25c0f776eb7be28d9ed6d7a2f07f40473
akicho8 <akicho8@gmail.com>
2025-10-02 19:07:46 +0900

    [feat] add assert_present, assert_blank

--- src/xassertion.js
+++ src/xassertion.js
@@ -1,4 +1,5 @@
 import _ from "lodash"
+import { Xobject } from "./xobject.js"
 
 export const Xassertion = {
   assert(value, message = null) {
@@ -15,24 +16,32 @@ export const Xassertion = {
     }
   },
 
-  assert_kind_of_integer(value, message = null) {
+  assert_kind_of_integer(value, message = "Assertion failed: not integer") {
     this.assert(Number.isInteger(value))
   },
 
-  assert_kind_of_string(value, message = null) {
+  assert_kind_of_string(value, message = "Assertion failed: not string") {
     this.assert_equal("string", typeof value, message)
   },
 
-  assert_kind_of_array(value, message = null) {
+  assert_kind_of_array(value, message = "Assertion failed: not array") {
     this.assert(_.isArray(value), message)
   },
 
-  assert_kind_of_hash(value, message = null) {
+  assert_kind_of_hash(value, message = "Assertion failed: not hash") {
     this.assert_equal("object", typeof value, message)
   },
 
-  assert_kind_of_object(value, message = null) {
+  assert_kind_of_object(value, message = "Assertion failed: not object") {
     this.assert_equal("object", typeof value, message)
+  },
+
+  assert_present(value, message = "Assertion failed: blank") {
+    this.assert(Xobject.present_p(value), message)
+  },
+
+  assert_blank(value, message = "Assertion failed: present") {
+    this.assert(Xobject.blank_p(value), message)
   },
 
   assert_nonzero(v, message = "divided by 0") {
